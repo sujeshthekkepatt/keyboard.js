1ddf1a5713d77e4f6afd9bd27b076236fd7b548c
sujesh <sujesh@linways.com>
2017-10-04 16:00:05 +0530

    updated

--- keyboard.js
+++ keyboard.js
@@ -1,4 +1,32 @@
-(function(){
+function getKeyCode(param)
+{
+    //console.log(param);
 
-console.log("keyboard.js started");
-});
+var keycode={
+        enter:"13"
+
+
+
+
+}
+console.log(keycode[param]);
+
+}
+
+
+
+
+function keyBoard(param,callback)
+{
+
+   // console.log(param);
+console.log("hai keyboard just loaded initialy");
+if(typeof callback == "function")
+    {
+   console.log(getKeyCode(param))    ;
+    callback();
+    }
+}
+
+//window.onload=keyBoard;
+window.onkeypress=keyBoard;
